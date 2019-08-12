de93637356a39fcfacf3d940253bcd34baa9c53e
Suzi Rubino <suzi@suzirubi.com>
2019-08-11 20:03:51 -0700

    version with map array function

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,64 @@
-
-
 var items = ["item1", "item2", "item3", "item4", "item5"];
-var userItems = [];
 
-var itemListSort = function(){
-
-  items.forEach(function(item){
-    var upperCaseItem = $("input#" + item).val().toUpperCase();
-    userItems.push(upperCaseItem);
-  });
-
-  userItems.sort();
-
-  return userItems
-
-};
 
 $(document).ready(function(){
+  $("form#items").submit(function(event){
+    event.preventDefault();
 
- $("form#items").submit(function(event){
-   event.preventDefault();
+    $("ol").empty();
+    $("form#items").hide();
+    $("div#userList").show();
 
-  $("ol").empty();
-  $("form#items").hide();
-  $("div#userList").show();
+    var userItems = items.map(function(item){
+      var userItem = $("input#" + item).val();
+      return userItem.toUpperCase();
+    });
 
-  itemListSort();
-  
-  userItems.forEach(function(item){
-     $("ol").append("<li>" + item + "</li>");
+    userItems.sort();
+
+    userItems.forEach(function(item){
+       $("ol").append("<li>" + item + "</li>");
+    });
+
   });
 
- });
-
 });
+
+
+
+//
+//
+// var items = ["item1", "item2", "item3", "item4", "item5"];
+// var userItems = [];
+//
+// var itemListSort = function(){
+//
+//   items.forEach(function(item){
+//     var upperCaseItem = $("input#" + item).val().toUpperCase();
+//     userItems.push(upperCaseItem);
+//   });
+//
+//   userItems.sort();
+//
+//   return userItems
+//
+// };
+//
+// $(document).ready(function(){
+//
+//  $("form#items").submit(function(event){
+//    event.preventDefault();
+//
+//   $("ol").empty();
+//   $("form#items").hide();
+//   $("div#userList").show();
+//
+//   itemListSort();
+//
+//   userItems.forEach(function(item){
+//      $("ol").append("<li>" + item + "</li>");
+//   });
+//
+//  });
+//
+// });
